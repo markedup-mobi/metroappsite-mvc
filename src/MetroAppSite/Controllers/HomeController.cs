9e81cf9729d893c60a932e03180a8105d8e0b7e2
Aaron Stannard <aaron@aaronstannard.com>
2012-12-18 10:00:57 -0800

    Added a route on the home controller designed to make it easier for developers to specify their download url

--- src/MetroAppSite/Controllers/HomeController.cs
+++ src/MetroAppSite/Controllers/HomeController.cs
@@ -6,12 +6,26 @@ namespace MetroAppSite.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.Title = "MetroAppSite";
+
             return View();
         }
 
         public ActionResult Privacy()
         {
+            ViewBag.Title = "MetroAppSite - Privacy Policy";
+
             return View();
+        }
+
+        public ActionResult Download()
+        {
+            /*
+                ToDo: change this URL to be equal to the URL of your app on the Windows Store
+             */
+            var AppDownloadUrl = "http://apps.microsoft.com/windows/en-US/app/relay/c7913da4-33ca-4642-8844-8bfa30c1e524";
+
+            return RedirectPermanent(AppDownloadUrl);
         }
 
     }
